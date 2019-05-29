2c37e806fdf481761ba8bbf694c01b991e890140
Sho Sobue <meru26.sho@gmail.com>
2019-05-30 01:51:00 +0900

    とりあえず起動するアプリケーションのデプロイ

---
+++ src/main/java/jp/sobue/oauth2/provider/Oauth2ProviderApplication.java
@@ -0,0 +1,13 @@
+package jp.sobue.oauth2.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2ProviderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/jp/sobue/oauth2/provider/Oauth2ProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.sobue.oauth2.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ProviderApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
